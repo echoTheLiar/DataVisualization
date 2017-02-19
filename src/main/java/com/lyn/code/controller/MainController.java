a5bf7570a809849985aec09ebb94476832f22d34
Liu Yuning <liu_yuning@foxmail.com>
2017-02-19 21:38:53 +0800

    init project

---
+++ src/main/java/com/lyn/code/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.lyn.code.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/test")
+	public String test(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "web/test";
+	}
+
+}
