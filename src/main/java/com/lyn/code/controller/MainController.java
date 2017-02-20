5948f74e0078881092c54f1fafeee95cf1e10257
Liu <yuning.liu@sap.com>
2017-02-20 17:50:41 +0800

    use bootstrap

--- src/main/java/com/lyn/code/controller/MainController.java
+++ src/main/java/com/lyn/code/controller/MainController.java
@@ -1,13 +1,12 @@
 package com.lyn.code.controller;
 
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-	
+
 	@RequestMapping("/test")
 	public String test() {
 		return "web/test";
